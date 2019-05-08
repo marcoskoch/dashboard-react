e7acd8bd4176c8abb5b73e93c1e3223af2a8e460
Marcos Koch <marcos_koch@hotmail.com>
2019-05-07 23:20:30 -0300

    Revert "Adicionado theme from https://github.com/creativetimofficial/material-dashboard-react"
    This reverts commit df4b8da88decc69f7fe17a228e3fe65b0fd39051.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AppBar from './components/AppBar'
+
+const classes = {
+  appBar : AppBar
+}
+
+function App() {
+  return (
+    <AppBar />
+  );
+}
+
+export default App;
